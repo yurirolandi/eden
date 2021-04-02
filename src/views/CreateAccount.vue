90bb467beab9abdcde324fb39c2cf3145a5fc4ae
Rolandi <sd.rolandi@gmail.com>
2021-04-02 10:11:44 -0300

    [feat] - view account created

---
+++ src/views/CreateAccount.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="create-login">
+   
+  </div>
+</template>
+<script>
+export default {
+  name: "CreateAccount",
+  components: {  },
+};
+</script>
+<style scoped></style>
