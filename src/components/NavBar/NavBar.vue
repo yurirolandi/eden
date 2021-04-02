568636081c61d695d63c9ea327a8e1c54dac2854
Rolandi <sd.rolandi@gmail.com>
2021-04-02 08:08:04 -0300

    [feat] - navbar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,34 +1,94 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
+  <div class="navbar">
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-app-bar-nav-icon @click="toggleMenu" />
+      </div>
 
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
-      />
-    </div>
+      <v-spacer></v-spacer>
 
-    <v-spacer></v-spacer>
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
 
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
-  </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="../../assets/logo.png"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Nome</v-list-item-title>
+          <v-list-item-subtitle>Level</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          router
+          :to="item.route"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title
+              ><button @click.prevent="logout">
+                Logout
+              </button></v-list-item-title
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      items: [
+        { title: "Home", icon: "mdi-view-dashboard", route: "/" },
+        // { title: "Login", icon: "mdi-login", route: "/login" },
+        // { title: "About", icon: "mdi-forum", route: "/tools" },
+      ],
+      drawer: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.drawer = !this.drawer;
+    },
+    logout() {
+      console.log("deslogando");
+    },
+  },
+};
+</script>
