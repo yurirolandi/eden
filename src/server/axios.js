e59139985e8e499d1dd54bc603a566ee3364b30c
Rolandi <sd.rolandi@gmail.com>
2021-04-02 08:09:50 -0300

    [config] - eslint config

--- src/server/axios.js
+++ src/server/axios.js
@@ -1,6 +1,5 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 export const requestAxios = axios.create({
-    baseURL: ' http://localhost:3000/'
-})
+  baseURL: " http://localhost:3000/",
+});
