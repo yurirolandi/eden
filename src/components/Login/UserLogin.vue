ca177e986f24b69dd91135714a1103dda76f0e07
Rolandi <sd.rolandi@gmail.com>
2021-04-02 10:08:53 -0300

    [feat] - login component

---
+++ src/components/Login/UserLogin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <div class="lighten-5">
+      <UserForm />
+      <p class="pa-1 mt-3">
+        Ao continuar com o acesso, você concorda com a nossa <br />
+        política de privacidade
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserForm from "./UserForm";
+export default {
+  name: "UserLogin",
+  components: { UserForm },
+};
+</script>
+
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="login">
-    <h1>login</h1>
+    <div class="container">
+      <h1 class="text-center mt-5">login</h1>
+      <UserLogin />
+    </div>
   </div>
 </template>
 <script>
+import UserLogin from "../components/Login/UserLogin";
 export default {
   name: "Login",
+  components: { UserLogin },
 };
 </script>
 <style scoped></style>
