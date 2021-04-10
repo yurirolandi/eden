38afbaa37ca17028ab8ca4d65609bae3be2c5adf
Rolandi <sd.rolandi@gmail.com>
2021-04-10 15:54:50 -0300

    [feat] - estrutura base

---
+++ src/components/DashboardHome/HomeLogged/HomeLogged.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <DashboardHome />
+  </div>
+</template>
+
+<script>
+import DashboardHome from "../../CarroucelGraus/DashboardHome";
+export default {
+  components: {
+    DashboardHome,
+  },
+};
+</script>
