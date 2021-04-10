1a22a04421d2baf5634cc9a17dfba93c94604c46
Rolandi <sd.rolandi@gmail.com>
2021-04-10 15:55:25 -0300

    [feat] - create module

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,9 +12,16 @@
 import SectionBoasVindas from "../components/SectionHome/SectionBoasVindas";
 import SectionHistoria from "../components/SectionHome/SectionHistoria";
 import SectionGrau from "../components/SectionHome/SectionGraus";
+import { mapActions } from "vuex";
 export default {
   name: "Home",
   components: { SectionBoasVindas, SectionHistoria, SectionGrau },
+  methods: {
+    ...mapActions(["getColunas"]),
+  },
+  created() {
+    this.getColunas();
+  },
 };
 </script>
 
--- src/views/HomeLogado.vue
+++ src/views/HomeLogado.vue
@@ -1,14 +1,16 @@
 <template>
-       <div class="home">
-
-       </div>
+  <div class="home">
+    <HomeLogged />
+  </div>
 </template>
 <script>
+import HomeLogged from "../components/DashboardHome/HomeLogged/HomeLogged";
 export default {
-    
-}
+  components: {
+    HomeLogged,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
