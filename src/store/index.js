5d6479f10f3797bf0ec1b518fdf05083eb0b6bb2
Rolandi <sd.rolandi@gmail.com>
2021-04-10 15:54:59 -0300

    [feat] - module

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import Login from "./modules/Login";
+import GrausColunas from "./modules/GrausColunas";
 
 Vue.use(Vuex);
 
@@ -8,5 +9,5 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: { Login },
+  modules: { Login, GrausColunas },
 });
